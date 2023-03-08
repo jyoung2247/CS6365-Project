02bf96e06b7a7328741d9a36837a3dfc17b5d6c7
Jacob Young <jyoung325@gatech.edu>
2023-03-08 18:06:50 -0500

    Updates

--- steam_recommender.py
+++ steam_recommender.py
@@ -3,17 +3,30 @@ from surprise.model_selection import train_test_split
 import pandas as pd
 
 #Import our csv as a dataframe to use as data
-df = pd.read_csv("user-title-rating.csv")
-reader = Reader(rating_scale=(0,10))
-data = Dataset.load_from_df(df, reader=reader)
+def load_csv(filepath):
+    df = pd.read_csv(filepath)
+    reader = Reader(rating_scale=(0,10))
+    data = Dataset.load_from_df(df, reader=reader)
+    return data
 
+def fit_svd(train):
+    model = SVD()
+    model.fit(train)
+    return model
+    
+def model_rmse(model, test):
+    preds = model.test(test)
+    return accuracy.rmse(preds)
+
+#Predict how user will like title
+def get_prediction(uid, title):
+    prediction = model.predict(uid, title)
+    return prediction
+
+data = load_csv("user-title-rating.csv")
 #Create train test split
 train, test = train_test_split(data, test_size=.20)
-
-#Choose model, fit, and predict
-model = SVD()
-model.fit(train)
-preds = model.test(test)
-
-#Print accuracy
-print(accuracy.rmse(preds))
+model = fit_svd(train)
+rmse = model_rmse(model, test)
+prediction = get_prediction(59945701, "Dota 2")
+print(prediction)
