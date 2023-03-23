6930f56f4881b764d259aa60a2055195d6a54d3c
Jacob Young <jyoung325@gatech.edu>
2023-03-23 08:44:08 -0400

    Changed folder name

--- back-end/dataset_merging.py
+++ back-end/dataset_merging.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
 #Clean games
-df_games = pd.read_csv("original datasets/games.csv")
+df_games = pd.read_csv("original-datasets/games.csv")
 df_games = df_games.loc[:, ["name", "all_reviews", "release_date", "developer", "publisher", "game_details", "genre"]]
 #Create lower case game name column for the purpose of joins
 df_games["name_lower"] = df_games.name.str.lower()
 
 
 #Clean HLTB
-# df_hltb = pd.read_csv("original datasets/HLTB.csv")
+# df_hltb = pd.read_csv("original-datasets/HLTB.csv")
 # df_hltb = df_hltb.loc[:, ["title", "main_story"]]
 # #Strip h from main_story
 # df_hltb["main_story"] = df_hltb["main_story"].str.replace('h', '', regex=True).astype("float64")
@@ -30,7 +30,7 @@ df_metacritic["name_lower"] = df_metacritic.name.str.lower()
 df_metacritic.drop(columns="name", inplace=True)
 
 #Clean users
-df_users = pd.read_csv("original datasets/users.csv")
+df_users = pd.read_csv("original-datasets/users.csv")
 df_users = df_users[df_users.behavior == "play"]
 df_users = df_users.loc[:, ["uid", "title", "hours"]]
 #Create lower case game name column for the purpose of joins
--- back-end/update-HLTB.py
+++ back-end/update-HLTB.py
@@ -14,13 +14,13 @@ df_hltb_updated['main_story'] = df_hltb_updated['main_story'].apply(lambda x: re
 df_hltb_updated["name_lower"] = df_hltb_updated.title.str.lower()
 
 #Clean games
-df_games = pd.read_csv("original datasets/games.csv")
+df_games = pd.read_csv("original-datasets/games.csv")
 df_games = df_games.loc[:, ["name", "all_reviews", "release_date", "developer", "publisher", "game_details", "genre"]]
 #Create lower case game name column for the purpose of joins
 df_games["name_lower"] = df_games.name.str.lower()
 
 #Clean HLTB
-df_hltb = pd.read_csv("original datasets/HLTB.csv")
+df_hltb = pd.read_csv("original-datasets/HLTB.csv")
 df_hltb = df_hltb.loc[:, ["title", "main_story"]]
 #Strip non-digits from main_story
 df_hltb["main_story"] = df_hltb["main_story"].str.replace('h', '', regex=True).astype("float64")
