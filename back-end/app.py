38d6a02bca26d493e75ab4288baefa23eac93dc2
ethan <ethanwells@lawn-128-61-63-154.lawn.gatech.edu>
2023-04-07 17:58:28 -0400

    patch CORS Policy error

--- back-end/app.py
+++ back-end/app.py
@@ -2,8 +2,10 @@ from flask import Flask, jsonify, request
 import json
 from steam_recommender import Steam_Recommender
 import steam_api_functions
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello():
